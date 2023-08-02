231baac59828cd33234e535603882f367e713b3d
Yann Huberland <yhuberla@shi-c2-p3.s19.be>
2023-08-02 15:09:58 +0200

    docker-compose kinda working

--- backNest/src/app.module.ts
+++ backNest/src/app.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ServeStaticModule} from '@nestjs/serve-static'; // New
+// import { ServeStaticModule } from '@nestjs/serve-static'; // New
 import { join } from 'path'; // New
 
 
 @Module({
   imports: [
-    ServeStaticModule.forRoot({ // New
-      rootPath: '/usr/src/app/frontVue',//join(__dirname, '/../', 'frontVue'), // New
-    }), // New
+    // ServeStaticModule.forRoot({ // New
+      // rootPath: '/usr/src/app/frontVue',//join(__dirname, '/../', 'frontVue'), // New
+    // }), // New
   ],
   controllers: [AppController],
   providers: [AppService],
