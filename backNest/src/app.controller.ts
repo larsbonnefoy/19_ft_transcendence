efdb78a35422d26c8da04b374091d703610b13df
Yann Huberland <yhuberla@shi-c2-p3.s19.be>
2023-08-03 18:53:53 +0200

    playing around with controllers in nest

--- backNest/src/app.controller.ts
+++ backNest/src/app.controller.ts
@@ -1,13 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Param, Query, ParseIntPipe } from '@nestjs/common';
+// import { IsInt, IsString } from 'class-validator';
+// import { identity } from 'rxjs';
 import { AppService } from './app.service';
+
+// class GetParamType{
+//   @IsInt()
+//   id: number;
+
+//   @IsString()
+//   name: string;
+// }
+
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): object {
-    console.log("in default controller");
-    return this.appService.getJSON();
+  @Get(":name")
+  getHello(@Param() params: any, @Query('id', ParseIntPipe) id: Number): object {
+    console.log("in default controller with name %s and id %d", params.name, id);
+    return this.appService.getJSON(params.name, id);
   }
+  
 }
