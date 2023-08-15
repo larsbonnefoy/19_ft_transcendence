2d80c78d842cf215e3cd50ce30830b800400b400
Hugo Delmas <hdelmas@fu-r7-p4.s19.be>
2023-08-15 12:49:14 +0200

    added 2fa

--- frontVue/src/views/Auth.vue
+++ frontVue/src/views/Auth.vue
@@ -8,20 +8,6 @@ try
  	if (!urlParams.has('code'))
 		throw "node code in query";
 	code = urlParams.get('code');
-<<<<<<< HEAD
-	try {
-		const response : Response = await fetch(`http://localhost:3000/api42/getToken?code=${code}`);
-		const jwtToken : string = (await response.json())['jwt_token']
-		console.log(jwtToken)
-		//  get user id.
-		sessionStorage.setItem('jwt_token', jwtToken);
-		console.log("ended");
-		router.push('/home');
-	}
-	catch {
-		router.push("/");
-	}
-=======
 	const response : Response = await fetch(`http://localhost:3000/api42/getToken?code=${code}`);
 	if (response.status != 200 && response.status != 201)
 		throw "getToken failed";
@@ -34,7 +20,6 @@ try
 	sessionStorage.setItem('jwt_token', jwtToken);
 	console.log("ended");
     router.push('/home');
->>>>>>> hugo
 }
 catch (error)
 {
