25d3e66f867491077c66bf6b73cb349af0baedf9
Lars Bonnefoy <lars.bonnefoy@hotmail.com>
2023-08-17 13:39:20 +0200

    tried ws

--- frontVue/src/views/Chat.vue
+++ frontVue/src/views/Chat.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
 import Chat from '../components/Chat/Chat.vue'
+//const connection = new WebSocket("wss://echo.websocket.org")
 
+//console.log(connection);
 </script>
 
 <template>
+    <!-- 
     <Chat></Chat>
+    -->
 </template>
 
 <style>
