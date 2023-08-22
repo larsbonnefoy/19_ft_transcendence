2bfd2f79f35fa1d02b26deac67ae89cb5f70539f
GitHuberlandYann <yann.hub@hotmail.com>
2023-08-22 20:45:51 +0200

    first draft for the game, only the front side handles it for now

--- frontVue/src/views/Game.vue
+++ frontVue/src/views/Game.vue
@@ -4,106 +4,201 @@ import { useUserStore } from '@/stores/user';
 import { onMounted, onUnmounted, ref } from 'vue';
 const store = useUserStore();
 
-/* ATM ingame/online triggered when going to GAME menu */
-const gameCanvas = ref(null)
-
-
-
-
 /* GAME */
-var myGameArea = {
-  canvas : document.createElement("canvas"),
-  interval : setInterval(updateGameArea, 20),
-  key : 0,
-  start : function() {
-      this.canvas.width = 480;
-      this.canvas.height = 270;
-      this.canvas.style.backgroundColor = "#009090";
-      document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-      window.addEventListener('keydown', function (e: any) {
-          myGameArea.key = e.keyCode;
-        });
-      window.addEventListener('keyup', function (e: any) {
-          myGameArea.key = 0;
-        });
-  },
-  clear : function() {
-    var ctx = this.canvas.getContext("2d");
-    if (ctx) {
-        ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        ctx.strokeStyle = "black";
+const canvasWidth = 800;
+const canvasHeight = 600;
+const ballRadius = 10;
+const paddleWidth = 2 * ballRadius;
+const paddleHeight = 8 * ballRadius;
+const key_d = 68;
+const key_e = 69;
+const key_s = 83;
+const key_w = 87;
+
+
+let canvas: HTMLCanvasElement | any = null;
+let ctx: any = null;
+let key: number = 0;
+let startDirection = 5;
+let score0: number = 0;
+let score1: number = 0;
+let leftPaddle = {
+	x : paddleWidth,
+	y : canvasHeight / 2,
+	width : paddleWidth,
+	height : paddleHeight,
+	speedy : 0,
+	color : "blue",
+	update : function(){
+      this.y += this.speedy * 10;
+      if (this.y > canvas.height - this.height / 2) {
+          this.y = canvas.height - this.height / 2;
+	  } else if (this.y < this.height / 2) {
+		  this.y = this.height / 2;
+      }
+    },
+    display : function(){
+	  if (ctx) {
         ctx.beginPath();
-        ctx.rect(0, 0, this.canvas.width, this.canvas.height);
-        ctx.stroke();
+		ctx.rect(this.x - this.width / 2, this.y - this.height / 2, this.width, this.height);
+		ctx.fillStyle = this.color;
+        ctx.fill();
+        ctx.closePath();
+      }
+    },
+	ballCollision : function() : boolean {
+		return (myBall.x > this.x - this.width / 2 - myBall.radius
+			&& myBall.x < this.x + this.width / 2 + myBall.radius
+			&& myBall.y > this.y - this.height / 2 - myBall.radius
+			&& myBall.y < this.y + this.height / 2 + myBall.radius);
+	}
+};
+let rightPaddle = {
+	x : canvasWidth - paddleWidth,
+	y : canvasHeight / 2,
+	width : paddleWidth,
+	height : paddleHeight,
+	speedy : 0,
+	color : "blue",
+	update : function(){
+      this.y += this.speedy * 10;
+      if (this.y > canvas.height - this.height / 2) {
+          this.y = canvas.height - this.height / 2;
+	  } else if (this.y < this.height / 2) {
+		  this.y = this.height / 2;
+      }
+    },
+    display : function(){
+	  if (ctx) {
+        ctx.beginPath();
+		ctx.rect(this.x - this.width / 2, this.y - this.height / 2, this.width, this.height);
+		ctx.fillStyle = this.color;
+        ctx.fill();
+        ctx.closePath();
+      }
+    },
+	ballCollision : function() : boolean {
+		return (myBall.x > this.x - this.width / 2 - myBall.radius
+			&& myBall.x < this.x + this.width / 2 + myBall.radius
+			&& myBall.y > this.y - this.height / 2 - myBall.radius
+			&& myBall.y < this.y + this.height / 2 + myBall.radius);
+	}
+};
+let myBall = {
+	x : canvasWidth / 2,
+	y : canvasHeight / 2,
+	radius : ballRadius,
+	speedx : startDirection,
+	speedy : 0,
+	color : "red",
+	update : function(){
+      this.x += this.speedx;
+      if (this.x > canvas.width - this.radius) {
+		//   this.x = 2 * (canvas.width - this.radius) - this.x;
+        //   this.speedx *= -1;
+		++score0;
+		resetPositions();
+      } else if (this.x < this.radius) {
+        //   this.x = 2 * this.radius - this.x;
+        //   this.speedx *= -1;
+		++score1;
+		resetPositions();
+      }
+      this.y += this.speedy;
+      if (this.y > canvas.height - this.radius) {
+          this.y = 2 * (canvas.height - this.radius) - this.y;
+          this.speedy *= -1;
+	  } else if (this.y < this.radius) {
+		  this.y = 2 * this.radius - this.y;
+          this.speedy *= -1;
+      }
+	  if (leftPaddle.ballCollision()) {
+		(this.x > leftPaddle.x) ? this.speedx = 5 : this.speedx = -5;
+		this.speedy = (this.y - leftPaddle.y) * 5 / leftPaddle.height;
+	} else if (rightPaddle.ballCollision()) {
+		(this.x > rightPaddle.x) ? this.speedx = 5 : this.speedx = -5;
+		this.speedy = (this.y - rightPaddle.y) * 5 / rightPaddle.height;
+	  }
+    },
+    display : function(){
+	  if (ctx) {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+		ctx.fillStyle = this.color;
+        ctx.fill();
+        ctx.closePath();
+      }
     }
-  }
 };
 
-var myBall = {width: 50, height: 50, x : 0, y : 0, speedx: 5, speedy: -2,
-  update : function(){
-      var newx = this.x + this.speedx;
-      if (newx > myGameArea.canvas.width - 10) {
-          newx = myGameArea.canvas.width - (newx - myGameArea.canvas.width)
-          this.speedx *= -1;
-      } else if (newx < 10) {
-          newx *= -1;
-          this.speedx *= -1;
-      }
-      this.x = newx;
-      var newy = this.y + this.speedy;
-      if (newy > myGameArea.canvas.height - 10) {
-          newy = myGameArea.canvas.height - (newy - myGameArea.canvas.height)
-          this.speedy *= -1;
-      } else if (newy < 10) {
-          newy *= -1;
-          this.speedy *= -1;
-      }
-      this.y = newy;
-  },
-  display : function(){
-    var ctx = myGameArea.canvas.getContext("2d");
-    if (ctx) {
-        ctx.strokeStyle = "red";
-        ctx.beginPath();
-        for (let i=0; i < 10; i++)
-            ctx.arc(this.x - 5, this.y + 5, i, 0, 2 * Math.PI);
-        ctx.stroke(); 
-        // ctx.fillRect(this.x, this.y, this.width, this.heiht);
-    }
-  }
-};
-
-
-function startGame() {
-    myGameArea.start();
-    setBall(30, 30, 10, 120);
-    myBall.display();
-
+function init() {
+	canvas = document.getElementById('gameCanvas') as HTMLCanvasElement;
+    canvas.width = canvasWidth;
+    canvas.height = canvasHeight;
+    canvas.style.backgroundColor = "#FFFFFF";
+	ctx = canvas.getContext('2d');
+	ctx.font = "30px Arial";
+	
+	setInterval(updateGameArea, 20);
+	
+	window.addEventListener('keydown', function (e: any) {
+		key = e.keyCode;
+    });
+	window.addEventListener('keyup', function (e: any) {
+		key = 0;
+    });
 }
 
-function setBall(width: number, height: number, x: number, y: number) {
-  myBall.width = width;
-  myBall.height = height;
-  myBall.x = x; 
-  myBall.y = y;
+function resetPositions() {
+	startDirection *= -1;
+
+	myBall.x = canvasWidth / 2;
+	myBall.y = canvasHeight / 2;
+	myBall.speedx = startDirection;
+	myBall.speedy = 0;
+
+	leftPaddle.y = canvasHeight / 2;
+	leftPaddle.speedy = 0;
+	rightPaddle.y = canvasHeight / 2;
+	rightPaddle.speedy = 0;
+
+	key = 0;
 }
 
 function updateGameArea() {
-  myGameArea.clear();
-//   myBall.speedx = 0;
-//   myBall.speedy = 0;    
-//   if (myGameArea.key && myGameArea.key == 37) {myBall.speedx = -1; }
-//   if (myGameArea.key && myGameArea.key == 39) {myBall.speedx = 1; }
-//   if (myGameArea.key && myGameArea.key == 38) {myBall.speedy = -1; }
-//   if (myGameArea.key && myGameArea.key == 40) {myBall.speedy = 1; }
-  myBall.update();
-  myBall.display();
+	clearGameArea();
+	leftPaddle.speedy = 0;  
+	if (key == key_w) {leftPaddle.speedy = -1; }
+    if (key == key_s) {leftPaddle.speedy = 1; }
+	rightPaddle.speedy = 0;  
+	if (key == key_e) {rightPaddle.speedy = -1; }
+    if (key == key_d) {rightPaddle.speedy = 1; }
+
+	leftPaddle.update();
+	leftPaddle.display();
+	rightPaddle.update();
+	rightPaddle.display();
+	myBall.update();
+	myBall.display();
+
+	ctx.fillStyle = "black";
+	ctx.fillText(score0, canvasWidth / 4, canvasHeight / 8);
+	ctx.fillText(score1, 3 * canvasWidth / 4, canvasHeight / 8);
 }
 
+function clearGameArea() {
+	if (ctx) {
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		ctx.strokeStyle = "black";
+		ctx.beginPath();
+		ctx.rect(0, 0, canvas.width, canvas.height);
+		ctx.stroke();
+	}
+}
 
 onMounted(async () => {
     await store.setStatus("ingame");
-    startGame();
+    init();
 })
 
 onUnmounted(async () => {
@@ -113,8 +208,27 @@ onUnmounted(async () => {
 
 <template>
     <h1> Game Page </h1>
-    <!-- <canvas id="c-game-canvas" ref="gameCanvas"></canvas> -->
-    <!-- <script src="game.js"></script>-->
+
+	<!-- <div class="container py-5 h-100"> -->
+    <!-- <div class="row d-flex justify-content-center align-items-center"> -->
+    <!-- <div class="row align-items-center"> -->
+		<!-- <div class="col m-5"> -->
+			<!-- <canvas id="gameCanvas"></canvas> -->
+		<!-- </div> -->
+	<!-- </div> -->
+	<!-- </div> -->
+		
+	<div class="row align-items-center">
+		<canvas id="gameCanvas"></canvas>	
+	</div>
+	
+	<!-- <div class="container-fluid"> -->
+	<!-- <div class="row align-items-center">
+		<div class="col-2">1 of 3</div>
+		<div class="col-8"><canvas id="gameCanvas"></canvas></div>
+		<div class="col-2">3 of 3</div>
+	</div> -->
+	<!-- </div> -->
 </template>
 
 <style>
