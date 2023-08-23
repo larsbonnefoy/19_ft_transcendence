bb746a959eb1514c70533fc384dc19e0df59e854
Yann Huberland <yhuberla@shi-c2-p3.s19.be>
2023-08-23 17:40:50 +0200

    game operational on back, comm with sockets to front

--- backNest/src/match/match.gateway.ts
+++ backNest/src/match/match.gateway.ts
@@ -4,49 +4,165 @@ import {
     WebSocketGateway,
     WebSocketServer,
     WsResponse,
-  } from '@nestjs/websockets';
-  import { Res } from '@nestjs/common';
-  import { from, Observable } from 'rxjs';
-  import { map } from 'rxjs/operators';
-  import { Server } from 'socket.io';
-  import { Response } from 'express';
+} from '@nestjs/websockets';
+import { Res } from '@nestjs/common';
+import { from, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Server } from 'socket.io';
+import { Response } from 'express';
 
+const canvasWidth : number = 800;
+const canvasHeight : number = 600;
+const ballRadius : number = 10;
+const ballSpeed : number = 10; //speed should be at least > 2
+
+let game = {
+    ongoing : false,
+    startDirection : 1,
+    score0 : 0,
+    score1 : 0,
+    background : "black",
+    leftPaddle : {
+      x : 2 * ballRadius,
+      y : canvasHeight / 2,
+      width : 2 * ballRadius,
+      height : 8 * ballRadius,
+      color : "white",
+      ballCollision : function() : boolean {
+          return (game.ball.x > this.x - this.width / 2 - game.ball.radius
+              && game.ball.x < this.x + this.width / 2 + game.ball.radius
+              && game.ball.y > this.y - this.height / 2 - game.ball.radius
+              && game.ball.y < this.y + this.height / 2 + game.ball.radius);
+      }
+    },
+    rightPaddle : {
+      x : canvasWidth - 2 * ballRadius,
+      y : canvasHeight / 2,
+      width : 2 * ballRadius,
+      height : 8 * ballRadius,
+      color : "white",
+      ballCollision : function() : boolean {
+        return (game.ball.x > this.x - this.width / 2 - game.ball.radius
+            && game.ball.x < this.x + this.width / 2 + game.ball.radius
+            && game.ball.y > this.y - this.height / 2 - game.ball.radius
+            && game.ball.y < this.y + this.height / 2 + game.ball.radius);
+      }
+    },
+    ball : {
+      x : canvasWidth / 2,
+      y : canvasHeight / 2,
+      radius : ballRadius,
+      speed : ballSpeed,
+      speedx : ballSpeed,
+      speedy : 0,
+      color : "white",
+      update : function(){
+        this.x += this.speedx;
+        if (this.x > canvasWidth - this.radius) {
+            //   this.x = 2 * (canvasWidth - this.radius) - this.x;
+            //   this.speedx *= -1;
+            ++game.score0;
+            resetPositions();
+        } else if (this.x < this.radius) {
+            //   this.x = 2 * this.radius - this.x;
+            //   this.speedx *= -1;
+            ++game.score1;
+            resetPositions();
+        }
+        this.y += this.speedy;
+        if (this.y > canvasHeight - this.radius) {
+            this.y = 2 * (canvasHeight - this.radius) - this.y;
+            this.speedy *= -1;
+        } else if (this.y < this.radius) {
+            this.y = 2 * this.radius - this.y;
+            this.speedy *= -1;
+        }
+        if (game.leftPaddle.ballCollision()) {
+            this.speedy = (this.y - game.leftPaddle.y) * (this.speed - 2) / (game.leftPaddle.height / 2);
+            (this.x > game.leftPaddle.x) ? this.speedx = this.speed : this.speedx = - this.speed;
+        } else if (game.rightPaddle.ballCollision()) {
+            this.speedy = (this.y - game.rightPaddle.y) * (this.speed - 2) / (game.rightPaddle.height / 2);
+            (this.x > game.rightPaddle.x) ? this.speedx = this.speed : this.speedx = - this.speed;
+        }
+      },
+    },
+}
+
+function resetPositions() {
+  game.startDirection *= -1;
   
-  @WebSocketGateway({
-    cors: {
-      origin: "http://localhost:5173",
-      methods: ["GET", "POST"],
+  game.ball.x = canvasWidth / 2;
+  game.ball.y = canvasHeight / 2;
+  game.ball.speedx = game.startDirection * ballSpeed;
+  game.ball.speedy = 0;
+    
+  game.leftPaddle.y = canvasHeight / 2;
+  game.rightPaddle.y = canvasHeight / 2;
+}
+
+function updateGameArea() {
+    game.ball.update();
+}
+
+@WebSocketGateway({
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
 //      allowedHeaders: ["my-custom-header"],
 //      credentials: true
-    },
-  })
-  export class MatchGateway {
-    @WebSocketServer()
-    server: Server;
-  
-    @SubscribeMessage('events')
-    findAll(@MessageBody() data: any): any /*Observable<WsResponse<number>>*/ {
-        console.log("got message: " + data);
-      //return from([1, 2, 3]).pipe(map(item => ({ event: 'events', data: item })));
-      //res.json({"test": "salut"});
-      this.server.emit("events", data);
-      return data;
-    }
-  
-    @SubscribeMessage('identity')
-    async identity(@MessageBody() data: number): Promise<number> {
-      return data;
-    }
-    
-    @SubscribeMessage('leftPaddle')
-    async computeLeftPaddle(@MessageBody() data: any) {
-        console.log("leftPaddle: " + data.pos);
-        if (data.pos > data.max) {
-            data.pos = data.max;
-        } else if (data.pos < data.min) {
-            data.pos = data.min;
-        }
-        this.server.emit("leftPaddle", data.pos);
-    }
+  },
+})
+export class MatchGateway {
+  @WebSocketServer()
+  server: Server;
 
+  @SubscribeMessage('events')
+  findAll(@MessageBody() data: any): any /*Observable<WsResponse<number>>*/ {
+      console.log("got message: " + data);
+    //return from([1, 2, 3]).pipe(map(item => ({ event: 'events', data: item })));
+    //res.json({"test": "salut"});
+    this.server.emit("events", data);
+    return data;
   }
+
+  @SubscribeMessage('identity')
+  async identity(@MessageBody() data: number): Promise<number> {
+    return data;
+  }
+  
+  @SubscribeMessage('leftPaddle')
+  async computeLeftPaddle(@MessageBody() dir: number) {
+      game.leftPaddle.y += 10 * dir;
+      // console.log("leftPaddle: " + <number>game.leftPaddle.y);
+      if (game.leftPaddle.y > canvasHeight - game.leftPaddle.height / 2) {
+          game.leftPaddle.y = canvasHeight - game.leftPaddle.height / 2;
+      } else if (game.leftPaddle.y < game.leftPaddle.height / 2) {
+          game.leftPaddle.y = game.leftPaddle.height / 2;
+      }
+  }
+
+  @SubscribeMessage('rightPaddle')
+  async computeRightPaddle(@MessageBody() dir: number) {
+      game.rightPaddle.y += 10 * dir;
+      // console.log("rightPaddle: " + <number>game.rightPaddle.y);
+      if (game.rightPaddle.y > canvasHeight - game.rightPaddle.height / 2) {
+        game.rightPaddle.y = canvasHeight - game.rightPaddle.height / 2;
+      } else if (game.rightPaddle.y < game.rightPaddle.height / 2) {
+        game.rightPaddle.y = game.rightPaddle.height / 2;
+      }
+  }
+
+  @SubscribeMessage('display')
+  async display() {
+    this.server.emit("display", game);
+  }
+
+  @SubscribeMessage('startGame')
+  async computeGame() {
+      if (!game.ongoing) {
+          game.ongoing = true;
+          setInterval(updateGameArea, 20);
+      }
+  }
+
+}
--- frontVue/src/components/Game/Canvas.vue
+++ frontVue/src/components/Game/Canvas.vue
@@ -7,10 +7,6 @@ const store = useUserStore();
 /* GAME */
 const canvasWidth = 800;
 const canvasHeight = 600;
-const ballRadius = 10;
-const ballSpeed = 10; //speed should be at least > 2
-const paddleWidth = 2 * ballRadius;
-const paddleHeight = 8 * ballRadius;
 const key_d = 68;
 const key_e = 69;
 const key_s = 83;
@@ -20,136 +16,16 @@ const key_w = 87;
 let canvas: HTMLCanvasElement | any = null;
 let ctx: any = null;
 let key: number = 0;
-let startDirection = 1;
-let scores = {
-    score0 : 0,
-    score1 : 0,
-    color : "white",
-    display : function() {
-        ctx.fillStyle = "white";
-        ctx.fillText(this.score0, canvasWidth / 4, canvasHeight / 8);
-        ctx.fillText(this.score1, 3 * canvasWidth / 4, canvasHeight / 8);
-    }
-};
-let leftPaddle = {
-    x : paddleWidth,
-	y : canvasHeight / 2,
-	width : paddleWidth,
-	height : paddleHeight,
-	speedy : 0,
-	color : "white",
-	update : function(){
-        this.y += this.speedy * 10;
-        if (this.y > canvas.height - this.height / 2) {
-            this.y = canvas.height - this.height / 2;
-        } else if (this.y < this.height / 2) {
-            this.y = this.height / 2;
-        }
-    },
-    display : function(){
-        if (ctx) {
-            ctx.beginPath();
-            ctx.rect(this.x - this.width / 2, this.y - this.height / 2, this.width, this.height);
-            ctx.fillStyle = this.color;
-            ctx.fill();
-            ctx.closePath();
-        }
-    },
-	ballCollision : function() : boolean {
-        return (myBall.x > this.x - this.width / 2 - myBall.radius
-            && myBall.x < this.x + this.width / 2 + myBall.radius
-            && myBall.y > this.y - this.height / 2 - myBall.radius
-            && myBall.y < this.y + this.height / 2 + myBall.radius);
-	}
-};
-let rightPaddle = {
-    x : canvasWidth - paddleWidth,
-	y : canvasHeight / 2,
-	width : paddleWidth,
-	height : paddleHeight,
-	speedy : 0,
-	color : "white",
-	update : function(){
-        this.y += this.speedy * 10;
-        if (this.y > canvas.height - this.height / 2) {
-            this.y = canvas.height - this.height / 2;
-        } else if (this.y < this.height / 2) {
-            this.y = this.height / 2;
-        }
-    },
-    display : function(){
-        if (ctx) {
-            ctx.beginPath();
-            ctx.rect(this.x - this.width / 2, this.y - this.height / 2, this.width, this.height);
-            ctx.fillStyle = this.color;
-            ctx.fill();
-            ctx.closePath();
-        }
-    },
-	ballCollision : function() : boolean {
-        return (myBall.x > this.x - this.width / 2 - myBall.radius
-            && myBall.x < this.x + this.width / 2 + myBall.radius
-            && myBall.y > this.y - this.height / 2 - myBall.radius
-            && myBall.y < this.y + this.height / 2 + myBall.radius);
-	}
-};
-let myBall = {
-    x : canvasWidth / 2,
-	y : canvasHeight / 2,
-	radius : ballRadius,
-	speed: ballSpeed, 
-	speedx : startDirection * ballSpeed,
-	speedy : 0,
-	color : "white",
-	update : function(){
-        this.x += this.speedx;
-        if (this.x > canvas.width - this.radius) {
-            //   this.x = 2 * (canvas.width - this.radius) - this.x;
-            //   this.speedx *= -1;
-            ++scores.score0;
-            resetPositions();
-        } else if (this.x < this.radius) {
-            //   this.x = 2 * this.radius - this.x;
-            //   this.speedx *= -1;
-            ++scores.score1;
-            resetPositions();
-        }
-        this.y += this.speedy;
-        if (this.y > canvas.height - this.radius) {
-            this.y = 2 * (canvas.height - this.radius) - this.y;
-            this.speedy *= -1;
-        } else if (this.y < this.radius) {
-            this.y = 2 * this.radius - this.y;
-            this.speedy *= -1;
-        }
-        if (leftPaddle.ballCollision()) {
-            this.speedy = (this.y - leftPaddle.y) * (this.speed - 2) / (leftPaddle.height / 2);
-            (this.x > leftPaddle.x) ? this.speedx = this.speed : this.speedx = - this.speed;
-        } else if (rightPaddle.ballCollision()) {
-            this.speedy = (this.y - rightPaddle.y) * (this.speed - 2) / (rightPaddle.height / 2);
-            (this.x > rightPaddle.x) ? this.speedx = this.speed : this.speedx = - this.speed;
-        }
-    },
-    display : function(){
-        if (ctx) {
-            ctx.beginPath();
-            ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
-            ctx.fillStyle = this.color;
-            ctx.fill();
-            ctx.closePath();
-        }
-    }
-};
+
 
 function init() {
+    socket.emit('startGame');
     canvas = document.getElementById('gameCanvas') as HTMLCanvasElement;
     canvas.width = canvasWidth;
     canvas.height = canvasHeight;
     canvas.style.backgroundColor = "#FFFFFF";
 	ctx = canvas.getContext('2d');
 	ctx.font = "30px Arial";
-	
-	setInterval(updateGameArea, 20);
 	
 	window.addEventListener('keydown', function (e: any) {
         key = e.keyCode;
@@ -158,78 +34,52 @@ function init() {
         key = 0;
     });
 
-    socket.on("leftPaddle", (y: any) => {
-        leftPaddle.y = y;
-        redrawAll();
-    });
-    socket.on("rightPaddle", (y: any) => {
-        rightPaddle.y = y;
-        redrawAll();
-    });
-}
+    socket.on('display', (response : any) => {
+        console.log(response.leftPaddle.y);
+        ctx.fillStyle = response.background;
+        ctx.beginPath();
+        ctx.fillRect(0, 0, canvasWidth, canvasHeight);
 
-function resetPositions() {
-    startDirection *= -1;
-    
-	myBall.x = canvasWidth / 2;
-	myBall.y = canvasHeight / 2;
-	myBall.speedx = startDirection * ballSpeed;
-	myBall.speedy = 0;
-    
-	socket.emit("leftPaddle", {"pos": canvasHeight / 2, "min": 0, "max": canvasHeight});
-	rightPaddle.y = canvasHeight / 2;
-	rightPaddle.speedy = 0;
-    
-	key = 0;
-}
+        ctx.beginPath();
+        ctx.rect(response.leftPaddle.x - response.leftPaddle.width / 2, response.leftPaddle.y - response.leftPaddle.height / 2, response.leftPaddle.width, response.leftPaddle.height);
+        ctx.fillStyle = response.leftPaddle.color;
+        ctx.fill();
+        ctx.closePath();
+        
+        ctx.beginPath();
+        ctx.rect(response.rightPaddle.x - response.rightPaddle.width / 2, response.rightPaddle.y - response.rightPaddle.height / 2, response.rightPaddle.width, response.rightPaddle.height);
+        ctx.fillStyle = response.rightPaddle.color;
+        ctx.fill();
+        ctx.closePath();
 
-function updateGameArea() {
-    clearGameArea();
-	leftPaddle.speedy = 0;
-	if (key == key_w) {
-        socket.emit("leftPaddle", {"pos": leftPaddle.y - 10,
-                                    "min": leftPaddle.height / 2,
-                                    "max": canvas.height - leftPaddle.height / 2});
-    }
-    if (key == key_s) {
-        socket.emit("leftPaddle", {"pos": leftPaddle.y + 10,
-                                    "min": leftPaddle.height / 2,
-                                    "max": canvas.height - leftPaddle.height / 2});
-    }
-	rightPaddle.speedy = 0;  
-	if (key == key_e) {rightPaddle.speedy = -1; }
-    if (key == key_d) {rightPaddle.speedy = 1; }
-    
-	leftPaddle.update();
-	leftPaddle.display();
-	rightPaddle.update();
-	rightPaddle.display();
-	myBall.update();
-	myBall.display();
-    
-	scores.display();
+        ctx.beginPath();
+        ctx.arc(response.ball.x, response.ball.y, response.ball.radius, 0, 2 * Math.PI);
+        ctx.fillStyle = response.ball.color;
+        ctx.fill();
+        ctx.closePath();
+
+        ctx.fillStyle = "white";
+        ctx.fillText(response.score0, canvasWidth / 4, canvasHeight / 8);
+        ctx.fillText(response.score1, 3 * canvasWidth / 4, canvasHeight / 8);
+    });
+
+    setInterval(redrawAll, 20);
 }
 
 function redrawAll() {
-    clearGameArea();
-    leftPaddle.display();
-    rightPaddle.display();
-	myBall.display();
-    scores.display();
-}
-
-function clearGameArea() {
-    if (ctx) {
-        // ctx.clearRect(0, 0, canvas.width, canvas.height);
-		// ctx.strokeStyle = "black";
-		// ctx.beginPath();
-		// ctx.rect(0, 0, canvas.width, canvas.height);
-		// ctx.stroke();
-
-        ctx.fillStyle = "black";
-        ctx.beginPath();
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
-	}
+    if (key == key_w) {
+        socket.emit("leftPaddle", -1);
+    }
+    if (key == key_s) {
+        socket.emit("leftPaddle", 1);
+    }
+    if (key == key_e) {
+        socket.emit("rightPaddle", -1);
+    }
+    if (key == key_d) {
+        socket.emit("rightPaddle", 1);
+    }
+    socket.emit('display');
 }
 
 onMounted(async () => {
