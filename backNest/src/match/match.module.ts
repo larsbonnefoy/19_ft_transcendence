5294045665ff0c44c45c1a2c14e95d1be9e2978c
Yann Huberland <yhuberla@shi-c2-p3.s19.be>
2023-08-23 12:21:46 +0200

    mini socket on back

--- backNest/src/match/match.module.ts
+++ backNest/src/match/match.module.ts
@@ -6,10 +6,11 @@ import { UserService } from '../user/user.service';
 import { Match } from './match.entity';
 import { MatchService } from './match.service';
 import { MatchController } from './match.controller';
+import { MatchGateway } from './match.gateway';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User, Match])],
   controllers: [MatchController],
-  providers: [UserService, MatchService],
+  providers: [UserService, MatchService, MatchGateway],
 })
 export class MatchModule {}
