ee36e936355421e4ef43201e5e44b0164877d12f
Hugo Delmas <hdelmas@fu-r8-p1.s19.be>
2023-08-24 15:52:14 +0200

    chat endpoints almost done

--- backNest/src/chat/chat.controller.ts
+++ backNest/src/chat/chat.controller.ts
@@ -39,8 +39,6 @@ export class ChatController {
     async getAll()
     {
         console.log("ALL");
-        // return await this.chatService.getMessages("test");
-		console.log(await this.chatService.getMessagesByUser("hdelmas"));
         return (await this.chatService.findAll());
     }
 
@@ -49,6 +47,7 @@ export class ChatController {
     {
         const roomId: string = params.roomId.slice(1);
 		//TODO check for password and user 
+        //TODO ADD FILTER FOR BLOCKER USER
         console.log("getMessage " + roomId);
 		return (await this.chatService.getMessagesByRoom(roomId));
     }
@@ -59,17 +58,298 @@ export class ChatController {
 		console.log("allAdmins")
 		return (await this.chatService.getAdmins(roomInfos.id));
 	}
+    @Post('allBans')
+    async getBans(@Body() roomInfos: roomDto)
+	{
+		console.log("allBans")
+		return (await this.chatService.getBans(roomInfos.id));
+	}
+	@Post('owner')
+    async getOwner(@Body() roomInfos: roomDto)
+	{
+		console.log("getOwner");
+		return (await this.chatService.getOwner(roomInfos.id));
+	}
+
 
 	@UseGuards(AuthGuard)
 	@Post('addAdmin')
-	async addAdmin(@Body() body: string, @Request() req: any)
+	async addAdmin(@Res() res: any, @Body() body: string, @Request() req: any)
 	{
+        //get room id
+        const roomId: string = body['id'];
+        if (!(await this.chatService.findOne(roomId)))
+        {
+            res.status(409).json({"error":"no chat room with that id"}).send();
+            return;
+        }
+
+        if (!body['newAdmin'])
+        {
+            res.status(400).json({"error":"Bad Request"}).send();
+            return;
+        }
+        //check if user is owner or admin
+        const agent: User | null = (await this.userService.findOne(req.user));
+        if (!(await this.chatService.isOwner(roomId, agent)) && !(await this.chatService.isAdmin(roomId, agent)))
+        {
+            await res.status(403).json({"error":"Forbidden"}).send();
+            return;
+        }
+        //create the user object to add
 		console.log("addAdmin");
 		const newAdmin: User = await this.userService.findOne(body['newAdmin']);
+
 		console.log(newAdmin)
-		this.chatService.addAdmin(body['id'], newAdmin);
+
+        //check is the User exist
+        if (!newAdmin)
+        {
+            res.status(409).json({"error":"no user with that id"}).send();
+            return;
+        }
+
+		await this.chatService.addAdmin(roomId, newAdmin);
+        await res.status(200).json({"status":"good"}).send();
+        return;
 	}
 
+    @UseGuards(AuthGuard)
+	@Post('delAdmin') 
+    async delAdmin(@Res() res: any, @Body() body: string, @Request() req: any)
+	{
+        //get room id
+        const roomId: string = body['id'];
+        if (!(await this.chatService.findOne(roomId)))
+        {
+            res.status(409).json({"error":"no chat room with that id"}).send();
+            return;
+        }
+
+        //check if user is owner or admin
+        const agent: User | null = (await this.userService.findOne(req.user));
+        if (!(await this.chatService.isOwner(roomId, agent)) && !(await this.chatService.isAdmin(roomId, agent)))
+        {
+            await res.status(403).json({"error":"Forbidden"}).send();
+            return;
+        }
+		console.log("delAdmin");
+        console.log(body['admin'])
+
+		await this.chatService.removeAdmin(roomId, body['admin']);
+        await res.status(200).json({"status":"good"}).send();
+        return;
+	}
+    
+	@UseGuards(AuthGuard)
+	@Post('addBan')
+	async addBan(@Res() res: any, @Body() body: string, @Request() req: any)
+	{
+        //get room id
+        const roomId: string = body['id'];
+        if (!(await this.chatService.findOne(roomId)))
+        {
+            res.status(409).json({"error":"no chat room with that id"}).send();
+            return;
+        }
+        if (!body['newBan'])
+        {
+            res.status(400).json({"error":"Bad Request"}).send();
+            return;
+        }
+        //create the user object to add
+		console.log("addBan");
+		const newBan: User = await this.userService.findOne(body['newBan']);
+
+
+        //check is the User exist
+        if (!newBan)
+        {
+            res.status(409).json({"error":"no user with that id"}).send();
+            return;
+        }
+
+        //check if user is owner or ban
+        const agent: User | null = (await this.userService.findOne(req.user));
+        if (!(await this.chatService.isOwner(roomId, agent)) && !(await this.chatService.isAdmin(roomId, agent)) || (await this.chatService.isOwner(roomId, newBan)))
+        {
+            await res.status(403).json({"error":"Forbidden"}).send();
+            return;
+        }
+		await this.chatService.addBan(roomId, newBan);
+        await res.status(200).json({"status":"good"}).send();
+        return;
+	}
+
+    @UseGuards(AuthGuard)
+	@Post('delBan') 
+    async delBan(@Res() res: any, @Body() body: string, @Request() req: any)
+	{
+        //get room id
+        const roomId: string = body['id'];
+        if (!(await this.chatService.findOne(roomId)))
+        {
+            res.status(409).json({"error":"no chat room with that id"}).send();
+            return;
+        }
+
+        //check if user is owner or ban
+        const agent: User | null = (await this.userService.findOne(req.user));
+        // if (!(await this.chatService.isOwner(roomId, agent)) && !(await this.chatService.isBan(roomId, agent)))
+        if (!(await this.chatService.isOwner(roomId, agent)) && !(await this.chatService.isAdmin(roomId, agent)))
+        {
+            await res.status(403).json({"error":"Forbidden"}).send();
+            return;
+        }
+		console.log("delBan");
+        console.log(body['ban'])
+
+		await this.chatService.removeBan(roomId, body['ban']);
+        await res.status(200).json({"status":"good"}).send();
+        return;
+	}
+
+	@UseGuards(AuthGuard)
+	@Post('addMute')
+	async addMute(@Res() res: any, @Body() body: string, @Request() req: any)
+	{
+        //get room id
+        const roomId: string = body['id'];
+        if (!(await this.chatService.findOne(roomId)))
+        {
+            res.status(409).json({"error":"no chat room with that id"}).send();
+            return;
+        }
+        if (!body['newMute'])
+        {
+            res.status(400).json({"error":"Bad Request"}).send();
+            return;
+        }
+        //create the user object to add
+		console.log("addMute");
+		const newMute: User = await this.userService.findOne(body['newMute']);
+
+		console.log(newMute)
+
+        //check is the User exist
+        if (!newMute)
+        {
+            res.status(409).json({"error":"no user with that id"}).send();
+            return;
+        }
+
+        //check if user is owner or mute
+        const agent: User | null = (await this.userService.findOne(req.user));
+        // if (!(await this.chatService.isOwner(roomId, agent)) && !(await this.chatService.isMute(roomId, agent)))
+        if (!(await this.chatService.isOwner(roomId, agent)) && !(await this.chatService.isAdmin(roomId, agent)) || (await this.chatService.isOwner(roomId, newMute)))
+        {
+            await res.status(403).json({"error":"Forbidden"}).send();
+            return;
+        }
+
+		await this.chatService.addMute(roomId, newMute);
+        await res.status(200).json({"status":"good"}).send();
+        return;
+	}
+
+    @UseGuards(AuthGuard)
+	@Post('delMute') 
+    async delMute(@Res() res: any, @Body() body: string, @Request() req: any)
+	{
+        //get room id
+        const roomId: string = body['id'];
+        if (!(await this.chatService.findOne(roomId)))
+        {
+            res.status(409).json({"error":"no chat room with that id"}).send();
+            return;
+        }
+
+        //check if user is owner or mute
+        const agent: User | null = (await this.userService.findOne(req.user));
+        // if (!(await this.chatService.isOwner(roomId, agent)) && !(await this.chatService.isMute(roomId, agent)))
+        if (!(await this.chatService.isOwner(roomId, agent)) && !(await this.chatService.isAdmin(roomId, agent)))
+        {
+            await res.status(403).json({"error":"Forbidden"}).send();
+            return;
+        }
+		console.log("delMute");
+        console.log(body['mute'])
+
+		await this.chatService.removeMute(roomId, body['mute']);
+        await res.status(200).json({"status":"good"}).send();
+        return;
+	}
+
+	@UseGuards(AuthGuard)
+	@Post('addChatter')
+	async addChatter(@Res() res: any, @Body() body: string, @Request() req: any)
+	{
+        //get room id
+        const roomId: string = body['id'];
+        if (!(await this.chatService.findOne(roomId)))
+        {
+            res.status(409).json({"error":"no chat room with that id"}).send();
+            return;
+        }
+
+        if (!body['newChatter'])
+        {
+            res.status(400).json({"error":"Bad Request"}).send();
+            return;
+        }
+        //check if user is owner or chatter
+        const agent: User | null = (await this.userService.findOne(req.user));
+        // if (!(await this.chatService.isOwner(roomId, agent)) && !(await this.chatService.isChatter(roomId, agent)))
+        if (!(await this.chatService.isOwner(roomId, agent)) && !(await this.chatService.isAdmin(roomId, agent)))
+        {
+            await res.status(403).json({"error":"Forbidden"}).send();
+            return;
+        }
+        //create the user object to add
+		console.log("addChatter");
+		const newChatter: User = await this.userService.findOne(body['newChatter']);
+
+		console.log(newChatter)
+
+        //check is the User exist
+        if (!newChatter)
+        {
+            res.status(409).json({"error":"no user with that id"}).send();
+            return;
+        }
+
+		await this.chatService.addChatter(roomId, newChatter);
+        await res.status(200).json({"status":"good"}).send();
+        return;
+	}
+
+    @UseGuards(AuthGuard)
+	@Post('delChatter') 
+    async delChatter(@Res() res: any, @Body() body: string, @Request() req: any)
+	{
+        //get room id
+        const roomId: string = body['id'];
+        if (!(await this.chatService.findOne(roomId)))
+        {
+            res.status(409).json({"error":"no chat room with that id"}).send();
+            return;
+        }
+
+        //check if user is owner or chatter
+        const agent: User | null = (await this.userService.findOne(req.user));
+        if (!(await this.chatService.isOwner(roomId, agent)) && !(await this.chatService.isAdmin(roomId, agent)))
+        {
+            await res.status(403).json({"error":"Forbidden"}).send();
+            return;
+        }
+		console.log("delChatter");
+        console.log(body['chatter'])
+
+		await this.chatService.removeChatter(roomId, body['chatter']);
+        await res.status(200).json({"status":"good"}).send();
+        return;
+	}
+//TODO CHECK TOKEN TO GET THE RIGHT USER AND CHECK IF UER IS OWNER IN DELETE
     @Post('create')
     async createRoom(@Body() roomInfos: roomDto, @Request() req: Request)
     {
@@ -85,6 +365,7 @@ export class ChatController {
         }
         this.chatService.createRoom(chat);
     }
+
     @Post('delete')
     async DeleteRoom(@Body() roomInfos: roomDto, @Request() req: Request, @Res() res: any)
     {
--- backNest/src/chat/chat.service.ts
+++ backNest/src/chat/chat.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { flatten, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Chat, ChatMessage } from './chat.entity';
 import { Repository } from 'typeorm';
 import * as bcrypt from 'bcrypt';
 import { User } from '../user/user.entity';
+import { timeStamp } from 'console';
 
 @Injectable()
 export class ChatService 
@@ -31,7 +32,8 @@ export class ChatService
 
 	async getOwner(roomId: string) : Promise<User | null>
 	{
-		return (await this.findOne(roomId)).owner
+		const owner: User | null = await this.userRepository.findOne({ relations: {owned: true},  where: { owned: {id: roomId}}})	
+		return owner; 
 	}
 	
 	async getChatters(roomId: string) : Promise<User[] | null>
@@ -64,6 +66,70 @@ export class ChatService
 		return await this.chatMessageRepository.find({ relations: {user: true},  where: { user: {login42: userId}}})
 	}
 
+	// Checkers
+
+	async	isOwner(roomId: string, user: User) : Promise<boolean>
+	{
+		console.log("isOwner");
+		const owner: User | null = await this.getOwner(roomId);
+		console.log(owner);
+		console.log(user)
+		if (!owner || owner.login42 !== user.login42)
+		{
+			console.log("false");
+			return false
+		}
+		return (true);
+	}
+
+	async isAdmin(roomId: string, user: User) : Promise<boolean>
+	{
+		console.log("isAdmin");
+		const admins: User[] = await this.getAdmins(roomId);
+		if (!admins || !admins.find(it => { return it.login42 === user.login42}))
+		{
+			console.log("false");
+			return false;
+		}
+		return true;
+	}
+
+	async isChatter(roomId: string, user: User) : Promise<boolean>
+	{
+		console.log("isChatter");
+		const chatters: User[] = await this.getChatters(roomId);
+		if (!chatters || !chatters.find(it => { return it.login42 === user.login42}))
+		{
+			console.log("false");
+			return false;
+		}
+		return true;
+	}
+
+	async isMute(roomId: string, user: User) : Promise<boolean>
+	{
+		console.log("isMute");
+		const mutes: User[] = await this.getMutes(roomId);
+		if (!mutes || !mutes.find(it => { return it.login42 === user.login42}))
+		{
+			console.log("false");
+			return false;
+		}
+		return true;
+	}
+
+	async isBan(roomId: string, user: User) : Promise<boolean>
+	{
+		console.log("isBan");
+		const bans: User[] = await this.getBans(roomId);
+		if (!bans || !bans.find(it => { return it.login42 === user.login42}))
+		{
+			console.log("false");
+			return false;
+		}
+		return true;
+	}
+
 	// room management
 	async createRoom(chat: Chat): Promise<Chat>
 	{
@@ -82,7 +148,7 @@ export class ChatService
 		await this.chatRepository.update(roomId, {password: hash});
 	}
 
-	async removePassword(roomId: string, pass: string)
+	async removePassword(roomId: string)
 	{
 		await this.chatRepository.update(roomId, {password: null});
 	}
@@ -108,7 +174,7 @@ export class ChatService
 	}
 
 	
-	async addChatters(roomId: string, newChatter: User)
+	async addChatter(roomId: string, newChatter: User)
 	{
 		const chatters: User[] = await this.getChatters(roomId);
 		chatters.push(newChatter);
@@ -136,6 +202,71 @@ export class ChatService
 		chat.bans= bans;
 		await this.chatRepository.save(chat);
 	}
+
+	async removeAdmin(roomId : string, adminId: string)
+	{
+		const admins = await this.getAdmins(roomId);
+		const index: number = admins.findIndex(it => { return it.login42 === adminId})
+		if (index >= 0)
+		{
+			const newAdminsLeft = admins.slice(0, index);
+			const newAdminsRight = admins.slice(index + 1);
+			const newAdmins = newAdminsLeft.concat(newAdminsRight);
+			
+			const chat : Chat = await this.findOne(roomId);
+			chat.admins = newAdmins;
+			await this.chatRepository.save(chat);
+		}
+	}
+
+	async removeChatter(roomId : string, chatterId: string)
+	{
+		const chatters = await this.getChatters(roomId);
+		const index: number = chatters.findIndex(it => { return it.login42 === chatterId})
+		if (index >= 0)
+		{
+			const newChattersLeft = chatters.slice(0, index);
+			const newChattersRight = chatters.slice(index + 1);
+			const newChatters = newChattersLeft.concat(newChattersRight);
+			
+			const chat : Chat = await this.findOne(roomId);
+			chat.chatters = newChatters;
+			await this.chatRepository.save(chat);
+		}
+	}
+
+	async removeBan(roomId : string, banId: string)
+	{
+		const bans = await this.getBans(roomId);
+		const index: number = bans.findIndex(it => { return it.login42 === banId})
+		if (index >= 0)
+		{
+			const newBansLeft = bans.slice(0, index);
+			const newBansRight = bans.slice(index + 1);
+			const newBans = newBansLeft.concat(newBansRight);
+			
+			const chat : Chat = await this.findOne(roomId);
+			chat.bans = newBans;
+			await this.chatRepository.save(chat);
+		}
+	}
+
+	async removeMute(roomId : string, muteId: string)
+	{
+		const mutes = await this.getMutes(roomId);
+		const index: number = mutes.findIndex(it => { return it.login42 === muteId})
+		if (index >= 0)
+		{
+			const newMutesLeft = mutes.slice(0, index);
+			const newMutesRight = mutes.slice(index + 1);
+			const newMutes = newMutesLeft.concat(newMutesRight);
+			
+			const chat : Chat = await this.findOne(roomId);
+			chat.mutes = newMutes;
+			await this.chatRepository.save(chat);
+		}
+	}
+
 	// Message Management 
 
 	async addMessage(roomId: string, newMessage: ChatMessage): Promise<boolean>
