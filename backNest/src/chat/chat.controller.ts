e7b478e2cf238b8d9b97779007c83cbd0c752471
Delmas Hugo <delmas.hugo@gmail.com>
2023-08-25 00:31:36 +0200

    addition in chat controller in nest

--- backNest/src/chat/chat.controller.ts
+++ backNest/src/chat/chat.controller.ts
@@ -15,12 +15,11 @@ export class ChatController {
     
 	@UseGuards(AuthGuard)
     @Post('message')
-    async postMessage(@Request() req: any, @Body() messageInfos: messageDto, @Res() res: any)
+    async postMessage(@Request() req: any, @Body() messageInfos: messageDto, @Res() res: any)//TODO CHECK IF IS CHATTER NOT BAN NOR MUTED
     {
         console.log("POSTMESSAGE");
         const message : ChatMessage = new ChatMessage;
-        // message.chat = messageInfos.roomId;
-		// message.user = await this.userService.findOne("hdelmas");
+        
         message.message = messageInfos.message;
 		message.user = await this.userService.findOne(req.user);
         if (await this.chatService.addMessage(messageInfos.roomId, message))
@@ -64,6 +63,7 @@ export class ChatController {
 		console.log("allBans")
 		return (await this.chatService.getBans(roomInfos.id));
 	}
+
 	@Post('owner')
     async getOwner(@Body() roomInfos: roomDto)
 	{
@@ -71,6 +71,65 @@ export class ChatController {
 		return (await this.chatService.getOwner(roomInfos.id));
 	}
 
+//TODO CHECK TOKEN TO GET THE RIGHT USER AND CHECK IF UER IS OWNER IN DELETE
+	@UseGuards(AuthGuard)
+    @Post('create')
+    async createRoom(@Body() roomInfos: roomDto, @Request() req: any, @Res() res: any)
+    {
+        console.log("CREATE");
+        const chat : Chat = new Chat;
+        chat.messages =  [];
+        chat.owner = await this.userService.findOne(req.user);
+        chat.id = roomInfos.id;
+        if (roomInfos.password)
+		{
+            const hash = await bcrypt.hash(roomInfos.password, 10);
+            chat.password = hash;
+        }
+		if (!(await this.chatService.findOne(roomInfos.id)))
+		{
+        	this.chatService.createRoom(chat);
+        	await res.status(200).json({"status":"good"}).send();
+		
+		}
+		else
+        {
+            await res.status(409).json({"error":"chat id already taken"}).send();
+        }
+            return;
+    }
+
+	@UseGuards(AuthGuard)
+    @Post('delete')
+    async DeleteRoom(@Body() roomInfos: roomDto, @Request() req: any, @Res() res: any)
+    {
+        console.log("DELETE");
+		if (this.chatService.isOwner(roomInfos.id, req.user))
+		{
+			const chat = await this.chatService.findOne(roomInfos.id);
+			if (!chat)
+			{    
+				res.status(409).json({"error":"no chat with that id"}).send();
+				return ;
+			}
+			console.log(chat);
+			if (chat.password)
+			{
+			if (!await this.chatService.verifyPassword(roomInfos.id, roomInfos.password))
+			{
+				await res.status(403).json({"error":"Forbidden"}).send();
+				return;
+			}
+			}
+			await this.chatService.deleteRoom(chat.id);
+			await res.status(200).json({"status":"good"}).send();
+			console.log("yo");
+		}
+		else
+			await res.status(403).json({"error":"Forbidden"}).send();
+		
+		return;
+    } 
 
 	@UseGuards(AuthGuard)
 	@Post('addAdmin')
@@ -80,13 +139,13 @@ export class ChatController {
         const roomId: string = body['id'];
         if (!(await this.chatService.findOne(roomId)))
         {
-            res.status(409).json({"error":"no chat room with that id"}).send();
+            await res.status(409).json({"error":"no chat room with that id"}).send();
             return;
         }
 
         if (!body['newAdmin'])
         {
-            res.status(400).json({"error":"Bad Request"}).send();
+            await res.status(400).json({"error":"Bad Request"}).send();
             return;
         }
         //check if user is owner or admin
@@ -105,7 +164,7 @@ export class ChatController {
         //check is the User exist
         if (!newAdmin)
         {
-            res.status(409).json({"error":"no user with that id"}).send();
+ 			await res.status(409).json({"error":"no user with that id"}).send();
             return;
         }
 
@@ -159,10 +218,8 @@ export class ChatController {
         }
         //create the user object to add
 		console.log("addBan");
-		const newBan: User = await this.userService.findOne(body['newBan']);
-
-
         //check is the User exist
+		const newBan: User = await this.userService.findOne(body['newBan']);
         if (!newBan)
         {
             res.status(409).json({"error":"no user with that id"}).send();
@@ -192,10 +249,8 @@ export class ChatController {
             res.status(409).json({"error":"no chat room with that id"}).send();
             return;
         }
-
         //check if user is owner or ban
         const agent: User | null = (await this.userService.findOne(req.user));
-        // if (!(await this.chatService.isOwner(roomId, agent)) && !(await this.chatService.isBan(roomId, agent)))
         if (!(await this.chatService.isOwner(roomId, agent)) && !(await this.chatService.isAdmin(roomId, agent)))
         {
             await res.status(403).json({"error":"Forbidden"}).send();
@@ -228,19 +283,15 @@ export class ChatController {
         //create the user object to add
 		console.log("addMute");
 		const newMute: User = await this.userService.findOne(body['newMute']);
-
-		console.log(newMute)
-
+		// console.log(newMute)
         //check is the User exist
         if (!newMute)
         {
             res.status(409).json({"error":"no user with that id"}).send();
             return;
         }
-
         //check if user is owner or mute
         const agent: User | null = (await this.userService.findOne(req.user));
-        // if (!(await this.chatService.isOwner(roomId, agent)) && !(await this.chatService.isMute(roomId, agent)))
         if (!(await this.chatService.isOwner(roomId, agent)) && !(await this.chatService.isAdmin(roomId, agent)) || (await this.chatService.isOwner(roomId, newMute)))
         {
             await res.status(403).json({"error":"Forbidden"}).send();
@@ -349,46 +400,4 @@ export class ChatController {
         await res.status(200).json({"status":"good"}).send();
         return;
 	}
-//TODO CHECK TOKEN TO GET THE RIGHT USER AND CHECK IF UER IS OWNER IN DELETE
-    @Post('create')
-    async createRoom(@Body() roomInfos: roomDto, @Request() req: Request)
-    {
-        console.log("CREATE");
-        const chat : Chat = new Chat;
-        chat.messages =  [];
-        chat.owner = await this.userService.findOne("hdelmas");
-        chat.id = roomInfos.id;
-        if (roomInfos.password)
-		{
-            const hash = await bcrypt.hash(roomInfos.password, 10);
-            chat.password = hash;
-        }
-        this.chatService.createRoom(chat);
-    }
-
-    @Post('delete')
-    async DeleteRoom(@Body() roomInfos: roomDto, @Request() req: Request, @Res() res: any)
-    {
-        console.log("DELETE");
-        //TODO CHECK USER
-        const chat = await this.chatService.findOne(roomInfos.id);
-        if (!chat)
-        {    
-            res.status(409).json({"error":"no chat with that id"}).send();
-            return ;
-        }
-        console.log(chat);
-        if (chat.password)
-		{
-           if (!await this.chatService.verifyPassword(roomInfos.id, roomInfos.password))
-           {
-                await res.status(403).json({"error":"Forbidden"}).send();
-                return;
-           }
-        }
-        await this.chatService.deleteRoom(chat.id);
-        await res.status(200).json({"status":"good"}).send();
-        console.log("yo");
-        return;
-    } 
 }
